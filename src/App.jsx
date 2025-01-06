8e9fb301ad7a12d926620c464d7950edff109527
Eitan Elberg <eitan.elberg@consumrz.com>
2025-01-06 16:21:15 +0200

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Hero from "./components/Hero.jsx"
+import Highlights from "./components/Highlights.jsx"
+import Navbar from "./components/Navbar.jsx"
+
 const App = () => {
 
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hi there
-    </h1>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
