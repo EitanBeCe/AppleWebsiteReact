3aa1835cf87f0e8e37939bc81770d31d318dd6fe
Eitan Elberg <eitan.elberg@consumrz.com>
2025-02-17 12:11:07 +0200

    phone model

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero.jsx"
 import Highlights from "./components/Highlights.jsx"
 import Navbar from "./components/Navbar.jsx"
+import PhoneModel from "./components/PhoneModel.jsx"
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <PhoneModel /> 
     </main>
   )
 }
---
+++ src/components/Phone3dModelView.jsx
@@ -0,0 +1,8 @@
+
+const Phone3dModelView = () => {
+  return (
+    <div>Phone3dModelView</div>
+  )
+}
+
+export default Phone3dModelView
